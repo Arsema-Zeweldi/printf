32b65c0c3651ad8e198d095d6e1fdea251582535
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-19 12:45:42 -0700

    update

--- conversion_specifiers.c
+++ conversion_specifiers.c
@@ -56,3 +56,55 @@ int print_str(va_list arg)
 
 	return (c);
 }
+/**
+ * print_STR - prints STRINGS
+ * @arg: argument
+ * Return: int
+ */
+int print_STR(va_list arg)
+{
+	int i;
+	char *str = va_arg(arg, char*);
+
+	if (str == NULL)
+		str = "(null)";
+	else if (*str == '\0')
+		return (-1);
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			if (i < 16)
+				_putchar('0');
+
+			print_unsignedIntToHex(str[i], 'A');
+		}
+		else
+			_putchar(str[i]);
+	}
+
+	return (i);
+}
+/**
+ * print_unsigned - prints unsigned int
+ * @arg: argument
+ * Return: int
+ */
+int print_unsigned(va_list arg)
+{
+	int divisor = 1, i, resp;
+	unsigned int n = va_arg(arg, unsigned int);
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+	return (i + 1);
+}
