dff2b16d87747a5ff5777486ae5bb83a5e6a9a9a
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-19 05:29:14 -0700

    task 0

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _putchar - prints
+ * @c: character to be printed
+ * Return: int
+ */
+int _putchar(char c)
+{
+	return (wrtie(1, &c, 1));
+}
