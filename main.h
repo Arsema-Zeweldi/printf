bc17224ca3b12d6bbcd99db5bcdfa9fb7eb2acf7
Arsema-Zeweldi <arsugeb@gmail.com>
2022-04-20 09:13:45 -0700

    1

--- main.h
+++ main.h
@@ -1,35 +1,33 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
 int print_char(va_list arg);
-int print_int(va_list arg);
 int print_str(va_list arg);
-int print_unsigned(va_list arg);
 int print_percent(void);
-int print_unsigned_binary(va_list arg);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
 int print_oct(va_list arg);
 int print_unsignedIntToHex(unsigned int num, char _case);
 int print_hex_base(va_list arg, char _case);
 int print_hex(va_list arg);
 int print_HEX(va_list arg);
-int print_STR(va_list arg);
-void print_binary(unsigned int n, unsigned int *printed);
+int print_STR (va_list arg);
 
 /**
- * struct specifiersStruct - specifier definition
- * @specifiers: types
- * @printer: printer
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
  */
-typedef struct specifiersStruct
+typedef struct identifierStruct
 {
-	char *specifiers;
-	int (*printer)(va_list);
-} specifiersStruct;
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif
